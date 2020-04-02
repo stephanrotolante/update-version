791d776a6cb15a018e9d05b17ef93f7d4c0fbf0a
stephanrotolante <stephanrotolante@gmail.com>
2020-04-02 16:17:47 -0400

    update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const fs = require('fs');
+const axios = require('axios');
 
 const path = './package.json';
 try {
@@ -10,8 +11,13 @@ try {
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload.repository.url, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  // const payload = JSON.stringify(github.context.payload.repository.url, undefined, 2);
+  const { payload }= github.context;
+  const { url } = payload.repository;
+  axios.get(`${url}/package.json`)
+  .then( res => console.log(JSON.stringify(res)))
+  .catch(error => console.log(JSON.stringify(error)));
+  // console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
