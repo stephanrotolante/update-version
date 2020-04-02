9c20b1f46aeaa3e7b5a51bd71c00ecf80e1ea9e4
stephanrotolante <stephanrotolante@gmail.com>
2020-04-02 08:07:40 -0400

    test

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ const path = './package.json'
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
-  if (fs.existsSync(path)){
-      console.log( core.setOutput("time", 'exists'));
-  }
+  console.log(`Hello ${nameToGreet}!`);
+  // if (fs.existsSync(path)){
+  //     console.log( core.setOutput("time", 'exists'));
+  // }
   const time = (new Date()).toTimeString();
-//   core.setOutput("time", time);
+  core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
