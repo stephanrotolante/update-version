ed2595b5a19e2a9c0a2848d076b290d867a73e36
stephanrotolante <stephanrotolante@gmail.com>
2020-04-02 08:14:03 -0400

    test

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const fs = require('fs');
 
-const path = './package.json'
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
-  // if (fs.existsSync(path)){
-  //     console.log( core.setOutput("time", 'exists'));
-  // }
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
