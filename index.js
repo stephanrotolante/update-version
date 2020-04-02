05a558f59778fa6d33c953b3fdb7ffd0a899f0e6
stephanrotolante <stephanrotolante@gmail.com>
2020-04-02 16:09:09 -0400

    update action

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ try {
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  const payload = JSON.stringify(github.context.payload.repository.url, undefined, 2)
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
