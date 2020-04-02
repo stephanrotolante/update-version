ce6028b28b724fc1525a72a9dbd1ee0588fc6ec0
stephanrotolante <stephanrotolante@gmail.com>
2020-04-02 01:08:31 -0400

    jdfkdsf

--- action.js
+++ action.js
@@ -2,21 +2,15 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 
-// try {
-//     // `who-to-greet` input defined in action metadata file
-//     const nameToGreet = core.getInput('who-to-greet');
-//     console.log(`Hello ${nameToGreet}!`);
-//     const time = (new Date()).toTimeString();
-//     core.setOutput("time", time);
-//     // Get the JSON webhook payload for the event that triggered the workflow
-//     const payload = JSON.stringify(github.context.payload, undefined, 2)
-//     console.log(`The event payload: ${payload}`);
-//   } catch (error) {
-//     core.setFailed(error.message);
-//   }
-
 try {
-    console.log(process.env);
-} catch {
-    core.setFailed("something went wrong");
-}
+    // `who-to-greet` input defined in action metadata file
+    const package = core.getInput('package');
+    console.log(package);
+    // const time = (new Date()).toTimeString();
+    // core.setOutput("time", time);
+    // // Get the JSON webhook payload for the event that triggered the workflow
+    // const payload = JSON.stringify(github.context.payload, undefined, 2)
+    // console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
